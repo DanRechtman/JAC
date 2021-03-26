82fd48dec9088acf5b73d3561d58f2406d363c96
Dan Rechtman <dannyrechtman@gmail.com>
2021-03-25 22:04:59 -0400

    First Commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from pyautogui import click,position,leftClick
+import sys
+
+class Click(object):
+
+    def __init__(self,move=False):
+        self.move = move
+    def click(self):
+        if (self.move):
+            self.pos = position()
+            while self.pos == position():
+                click(clicks=10)
+        else:
+            #TODO: Implement the click function when move is false
+            print("Not implemented")
+
+c = Click(move=True)
+
+
+c.click()
+        
